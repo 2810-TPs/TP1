8a34049f4671d4dbc855a875a64a2dfca574bc4c
unknown <hibenk@polymtl.ca>
2018-10-22 09:46:41 -0400

    fonction chemin le plus court

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -27,44 +26,20 @@ public class Graphe {
 		this.sommets = sommets;
 	}
 	
-	public void creerGraphe(BufferedReader lecteur) throws IOException {
-		
-		String ligne = lecteur.readLine();
-		if (ligne.length() > 0) {
-			
-			if (ligne.length() == 3 || ligne.length() == 4) {
-				//Creer sommet
-				creerSommet(ligne);
-			}
-			else {
-				//Relier sommet avec arc
-				
-			}
-			
+	public void initialiserListe(Sommet source, Sommet destination){
+		ArrayList<Sommet> listeSommets = new ArrayList<Sommet>();
+			listeSommets.add(source);
+			plusCourtChemin(source,destination,listeSommets);
+	}
+	
+	public void plusCourtChemin(Sommet source, Sommet destination,ArrayList<Sommet> listeSommets){
+		while(!listeSommets.contains(destination)){ // tant que la destination n'est pas ajoute			
+			for(int i = 0; i < source.getChemins().size();i++){
+				if(!listeSommets.contains(source.getChemins().get(i).getSommet2()))
+					listeSommets.add(source.getChemins().get(i).getSommet2());
+				plusCourtChemin(source.getChemins().get(i).getSommet2(),destination, listeSommets);
+					//on ajoute les voisins
+				}
 		}
-		creerGraphe(lecteur);
-	}
-	
-	public void creerSommet(String ligne) {
-		String identifiantStr = "";
-		if (ligne.length() == 4)
-			identifiantStr = ligne.substring(0,2);
-		else
-			identifiantStr = ligne.substring(0,1);
-		
-		int identifiant = Integer.parseInt(identifiantStr);
-		boolean estRechargeable = (ligne.charAt(2) == '0') ? false : true;
-		Sommet nouveauSommet = new Sommet(identifiant,estRechargeable);
-		sommets.put(identifiant, nouveauSommet);
-	}
-	
-	public void relierSommet(String ligne) {
-		
-		String sommetStr = ligne.substring(0, 2);
-		//Si sommetStr contient une virgule
-		if (sommetStr.charAt(sommetStr.length()-1) == ',')
-			//decouper
-			sommetStr = sommetStr.substring(0, 1);
-	
 	}
 }
